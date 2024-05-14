8f14165e6a959893ec15d4ab8769639e75f7d10c
joelau94 <joelau94@gmail.com>
2024-05-14 10:47:38 -0700

    add worker

--- mingpt/pipeline.py
+++ mingpt/pipeline.py
@@ -37,16 +37,17 @@ class BlocksPipeline(nn.Module):
             device: torch.cuda.Stream(device)
             for device in devices
         }
-        
+
         self._first_blocks_inited = False
-        
+
     def init_first_blocks(self):
         if not self._first_blocks_inited:
             for device_id in range(self.num_devices):
                 device = self.devices[device_id]
                 block_id = self.blocks_per_stage * device_id
                 with torch.cuda.stream(self.params_copy_streams[device]):
-                    self.blocks[block_id] = self.blocks[block_id].to(device, non_blocking=False)
+                    self.blocks[block_id] = self.blocks[block_id].to(
+                        device, non_blocking=False)
             self._first_blocks_inited = True
 
     def forward(self, x: torch.Tensor):
@@ -61,7 +62,7 @@ class BlocksPipeline(nn.Module):
 
             params_copy_stream = self.params_copy_streams[device]
             compute_stream = self.compute_streams[device]
-            
+
             data_move_event = None
             if device != x.device:
                 with torch.cuda.stream(self.data_copy_streams[device]):
@@ -77,12 +78,110 @@ class BlocksPipeline(nn.Module):
                 compute_done_event.record(compute_stream)
 
             with torch.cuda.stream(params_copy_stream):
-                self.blocks[next_block_id] = self.blocks[next_block_id].to(device, non_blocking=True)
+                self.blocks[next_block_id] = self.blocks[next_block_id].to(
+                    device, non_blocking=True)
                 params_copy_stream.wait_event(compute_done_event)
-                self.blocks[block_id] = self.blocks[block_id].to("cpu", non_blocking=True)
-                
+                self.blocks[block_id] = self.blocks[block_id].to(
+                    "cpu", non_blocking=True)
+
         return x
 
 
 def create_gpu_devices(gpu_ids):
     return [torch.device(f"cuda:{i}") for i in gpu_ids]
+
+
+class RotatingBlocksStage(nn.Module):
+
+    def __init__(self, blocks: List[nn.Module], device, copy_stream=None):
+        super().__init__()
+        self.blocks = blocks
+        self.device = device
+
+        self.num_blocks = len(self.blocks)
+        assert self.num_blocks > 2
+
+        self.compute_stream = torch.cuda.Stream(device)
+        self.copy_stream = (copy_stream if copy_stream is not None else
+                            torch.cuda.Stream(device))
+
+        self._init_loaded = False
+
+    def init_load(self):
+        if not self._init_loaded:
+            with torch.cuda.stream(self.copy_stream):
+                self.blocks[0] = self.blocks[0].to(self.device,
+                                                   non_blocking=False)
+            self._init_loaded = True
+
+    def forward(self, x: torch.Tensor):
+        self.init_load()
+        for block_id in range(self.num_blocks):
+            next_block_id = (block_id + 1) % self.num_blocks
+
+            data_move_event = None
+            if self.device != x.device:
+                with torch.cuda.stream(self.copy_stream):
+                    x = x.to(self.device)
+                    data_move_event = torch.cuda.Event()
+                    data_move_event.record()
+
+            with torch.cuda.stream(self.compute_stream):
+                if data_move_event is not None:
+                    self.compute_stream.wait_event(data_move_event)
+                x = self.blocks[block_id](x)
+                compute_event = torch.cuda.Event()
+                compute_event.record(self.compute_stream)
+
+            with torch.cuda.stream(self.copy_stream):
+                self.blocks[next_block_id] = self.blocks[next_block_id].to(
+                    self.device, non_blocking=True)
+                self.copy_stream.wait_event(compute_event)
+                self.blocks[block_id] = self.blocks[block_id].to(
+                    "cpu", non_blocking=True)
+
+        return x
+
+
+class SingleBlockStage(nn.Module):
+
+    def __init__(self, block: nn.Module, device, compute_stream=None, copy_stream=None):
+        super().__init__()
+        self.block = block
+        self.device = device
+        self.compute_stream = (compute_stream if compute_stream is not None else torch.cuda.current_stream(device))
+        self.copy_stream = (copy_stream if copy_stream is not None else
+                            torch.cuda.Stream(device))
+
+        self._init_loaded = False
+
+    def init_load(self):
+        if not self._init_loaded:
+            with torch.cuda.stream(self.copy_stream):
+                self.block = self.block.to(self.device, non_blocking=False)
+            self.block_loaded = True
+
+    def forward(self, *args, **kwargs):
+        self.init_load()
+        data_move_event = None
+        if self.device != x.device:
+            with torch.cuda.stream(self.copy_stream):
+                x = x.to(self.device)
+                data_move_event = torch.cuda.Event()
+                data_move_event.record()
+
+        with torch.cuda.stream(self.compute_stream):
+            if data_move_event is not None:
+                self.compute_stream.wait_event(data_move_event)
+            return self.block(*args, **kwargs)
+
+
+class Pipeline:
+
+    def __init__(self, batches, stages, devices, copy_streams: Optional[List[torch.cuda.Stream]] = None):
+        self.batches = batches
+        self.stages = stages
+        self.devices = devices
+        self.copy_streams = copy_streams
+        
+    def schedules
---
+++ mingpt/worker.py
@@ -0,0 +1,71 @@
+from contextlib import contextmanager
+from threading import Thread
+import sys
+
+from queue import Queue
+from typing import List
+import torch
+
+
+class Pack:
+    def __init__(self, *args):
+        self.args = args
+        
+    def call(self, func):
+        return Pack(func(*self.args))
+    
+
+class Task:
+    def __init__(self, compute_fn):
+        self.compute_fn = compute_fn
+    
+    def compute(self):
+        return self.compute_fn()
+    
+
+def worker(in_queue: Queue, out_queue: Queue):
+    while True:
+        task = in_queue.get()
+        if task is None:
+            break
+        
+        try:
+            pack = task.compute()
+            out_queue.put((True, pack))
+        except Exception:
+            exc_info = sys.exc_info()
+            out_queue.put((False, exc_info))
+            
+    out_queue.put((False, None))
+    
+
+@contextmanager
+def spawn_device_workers(devices: List[torch.device]):
+    in_queues = []
+    out_queues = []
+    
+    for device in devices:
+        in_queue = Queue()
+        out_queue = Queue()
+        
+        t = Thread(target=worker, args=(in_queue, out_queue), daemon=True)
+        t.start()
+
+        in_queues.append(in_queue)
+        out_queues.append(out_queue)
+        
+    try:
+        yield (in_queues, out_queues)
+    finally:
+        for in_queue in in_queues:
+            in_queue.put(None)
+        
+        # Join running threads
+        running = set(out_queues)
+        while running:
+            out_queue = running.pop()
+            ok, info = out_queue.get()
+            if info[1] is None:
+                continue
+            
+            running.add(out_queue)
